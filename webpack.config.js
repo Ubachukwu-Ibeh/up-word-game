cbd2cfc19950c016a3fedf8683b9e639ce4be3ab
= <=>
2020-11-02 18:18:09 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,6 @@ const path = require('path');
 module.exports = {
     watch: true,
     mode: 'development',
-    // devServer: {
-    //     open: true,
-    //     contentBase: './public',
-    //     compress: true,
-    //     hot: true,
-    //     port: 3000,
-    //     publicPath: './public'
-    // },
     entry: {
         game: './public/GAME/javascript/entry.js',
         levels: './public/LEVELS/javascript/entry.js'
