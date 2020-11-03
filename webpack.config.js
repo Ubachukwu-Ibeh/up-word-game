3666bd0098b4e65c5217ca5068889e0463ab55a1
= <=>
2020-11-03 21:50:43 +0100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    watch: true,
-    mode: 'development',
     entry: {
         game: './public/GAME/javascript/entry.js',
         levels: './public/LEVELS/javascript/entry.js'
