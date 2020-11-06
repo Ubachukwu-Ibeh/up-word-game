4d690a81eff1a1985a2951690a52a51a3b35836a
= <=>
2020-11-06 09:06:13 +0100

    commit

--- public/HOME/javascript/gameMode.js
+++ public/HOME/javascript/gameMode.js
@@ -1,14 +1,13 @@
 "use strict";
-//SOLO
 const soloMode = document.getElementById("solo");
-const botMode = document.getElementById("bot");
 const art = document.getElementById("art");
 let i = 0;
-const loadArr = [art, soloMode, botMode];
+const loadArr = [art, soloMode];
+
 setTimeout(() => {
   const runLoad = setInterval(() => {
     //play loadscreen animations
-    if (i === 2) {
+    if (i === 1) {
       clearInterval(runLoad);
     }
     loadArr[i].classList.add("pop");
@@ -16,19 +15,8 @@ setTimeout(() => {
   }, 100);
 }, 200);
 
-const PICK_SOLO = () => {
+soloMode.addEventListener("click", () => {
   localStorage.setItem("bot", 1);
   soloMode.classList.add("shake");
-};
-soloMode.addEventListener("click", () => {
-  PICK_SOLO();
-});
-//BOT
-const PICK_BOT = () => {
-  localStorage.setItem("bot", 0);
-  botMode.classList.add("shake");
-};
-botMode.addEventListener("click", () => {
-  PICK_BOT();
 });
 export let a;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -10,37 +9,5 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve('./public', 'dist')
-    },
-    plugins:[
-        new WorkboxPlugin.GenerateSW()
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: [
-              'style-loader',
-              'css-loader',
-            ],
-          },
-          {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-              'file-loader',
-            ],
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use: [
-              'file-loader',
-            ],
-          },
-          {
-            test: /\.(mp3|wav)$/,
-            use: [
-                'file-loader',
-            ]
-        }
-        ],
-    },
+    }
 }
